9a59e9e266c4abd569aaad4225d5ab9369abba95
kaito027 <kaito.san0.1818@gmail.com>
2018-09-14 02:01:12 +0900

    fix-position

--- kansen/Assets/Scripts/Drag.cs
+++ kansen/Assets/Scripts/Drag.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
@@ -8,12 +9,18 @@ using UnityEngine.EventSystems;
 public class Drag : MonoBehaviour {
 
     Vector2 startPosition;
-    Rigidbody2D bk ;
+    Rigidbody2D bk;
     float speed = 1;
+    GameObject Arrow_image;
     bool Mobe = true;
 
 	// Use this for initialization
 	void Start () {
+
+        //GameObject pc = GameObject.Find("baikin_hana");
+        //GameObject arrow = GameObject.Find("baikin_hana").transform.Find("arrow").gameObject;
+        
+        
 
         var eventTrigger = GetComponent<EventTrigger>();
             var beginDragEntry = new EventTrigger.Entry();
@@ -28,7 +35,7 @@ public class Drag : MonoBehaviour {
             dragEntry.eventID = EventTriggerType.Drag;
             dragEntry.callback.AddListener(data =>
             {
-                OnDrag((PointerEventData)data);
+                OnDrag((PointerEventData)data,(GameObject)Arrow_image);
             });
             eventTrigger.triggers.Add(dragEntry);
 
@@ -36,7 +43,7 @@ public class Drag : MonoBehaviour {
             endDragEntry.eventID = EventTriggerType.EndDrag;
             endDragEntry.callback.AddListener(data =>
             {
-                OnEndDrag((PointerEventData)data);
+                OnEndDrag((PointerEventData)data,(GameObject)Arrow_image);
             });
             eventTrigger.triggers.Add(endDragEntry);
 		
@@ -56,26 +63,34 @@ public class Drag : MonoBehaviour {
             bk.velocity = Vector2.zero;
             Mobe = true;
         }
-
 	}
 
     void OnBeginDrag(PointerEventData data)
     {
         startPosition = data.position;
         speed = 1;
+        GameObject Arrow_image = new GameObject("Arrowimage");
+        Arrow_image.transform.parent = GameObject.Find("baikin_hana").transform;
+        Arrow_image.GetComponent<Transform>().transform.localScale = new Vector2(1,1);
+        Arrow_image.GetComponent<Transform>().transform.localPosition = new Vector2(0,5);
+        Arrow_image.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Images/UI/Combat/arrow");
+        Arrow_image.GetComponent<SpriteRenderer>().sortingOrder = 3;
+      
     }
 
-    void OnDrag(PointerEventData data)
+    void OnDrag(PointerEventData data,GameObject Arrow_image)
     {
         var diff = startPosition - data.position;
+        //Arrow_image.transform.rotation = 
     }
 
-    void OnEndDrag(PointerEventData data)
+    void OnEndDrag(PointerEventData data,GameObject Arrow_image)
     {
         bk = GetComponent<Rigidbody2D>();
         var diff = startPosition - data.position;
         bk.velocity = diff*speed;
         Mobe = false;
+        GameObject.Destroy(Arrow_image);
     }
 
 }
--- kansen/Assets/Scripts/PlayerCollition.cs
+++ kansen/Assets/Scripts/PlayerCollition.cs
@@ -15,130 +15,87 @@ public class PlayerCollition : MonoBehaviour {
 	void Update () {
 		
 	}
+     
+     public void GameClear() {
+        GameObject GameClear_image = new GameObject("GameClearimage");
+        GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
+        GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
+        GameClear_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
+        GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear&nextstage");
+        GameClear_image.GetComponent<Image>().preserveAspect = true;
+        GameClear_image.GetComponent<Image>().SetNativeSize();
+     }
+
+     public void GameOver() {
+        GameObject GameOver_image = new GameObject("GameOverimage");
+        GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
+        GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
+        GameOver_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
+        GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
+        GameOver_image.GetComponent<Image>().preserveAspect = true;
+        GameOver_image.GetComponent<Image>().SetNativeSize();
+     }
+
+     public void GoSelect() {
+        SceneManager.LoadScene("Select");
+     }
+
+     public void GoSelect_Bard() {
+        SceneManager.LoadScene("Select_Bard");
+     }
+
+     public void GoSelect_Cat() {
+        SceneManager.LoadScene("Select_Cat");
+     }
+
+     public void GoSelect_Human() {
+        SceneManager.LoadScene("Select_Human");
+     }
+
+     public void GoTitle() {
+        SceneManager.LoadScene("Title");
+     }
 
      private void OnTriggerEnter2D(Collider2D other) {
             if(other.gameObject.tag == "GB") {
-
-                    GameObject GameClear_image = new GameObject("GameClearimage");
-                    GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameClear_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear");
-                    GameClear_image.GetComponent<Image>().preserveAspect = true;
-                    GameClear_image.GetComponent<Image>().SetNativeSize();
+                    GameClear();
                     Invoke("GoSelect_Bard",1.5f);
-                    
             }
 
             if(other.gameObject.tag == "GE") {
-
-                    GameObject GameOver_image = new GameObject("GameOverimage");
-                    GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameOver_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
-                    GameOver_image.GetComponent<Image>().preserveAspect = true;
-                    GameOver_image.GetComponent<Image>().SetNativeSize();
+                    GameOver();
                     Invoke("GoSelect",1.5f);
 
             }
 
             if(other.gameObject.tag == "BB") {
-
-                    GameObject GameClear_image = new GameObject("GameClearimage");
-                    GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameClear_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear");
-                    GameClear_image.GetComponent<Image>().preserveAspect = true;
-                    GameClear_image.GetComponent<Image>().SetNativeSize();
+                    GameClear();
                     Invoke("GoSelect_Cat",1.5f);
-                    
             }
 
             if(other.gameObject.tag == "BE") {
-
-                    GameObject GameOver_image = new GameObject("GameOverimage");
-                    GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameOver_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
-                    GameOver_image.GetComponent<Image>().preserveAspect = true;
-                    GameOver_image.GetComponent<Image>().SetNativeSize();
+                    GameOver();
                     Invoke("GoSelect_Bard",1.5f);
-
             }
 
             if(other.gameObject.tag == "CB") {
-
-                    GameObject GameClear_image = new GameObject("GameClearimage");
-                    GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameClear_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear");
-                    GameClear_image.GetComponent<Image>().preserveAspect = true;
-                    GameClear_image.GetComponent<Image>().SetNativeSize();
-                    Invoke("GoSelect_Human",1.5f);
-                    
+                    GameClear();
+                    Invoke("GoSelect_Human",1.5f);       
             }
 
             if(other.gameObject.tag == "CE") {
-
-                    GameObject GameOver_image = new GameObject("GameOverimage");
-                    GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameOver_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
-                    GameOver_image.GetComponent<Image>().preserveAspect = true;
-                    GameOver_image.GetComponent<Image>().SetNativeSize();
+                    GameOver();
                     Invoke("GoSelect_Cat",1.5f);
-
             }
 
             if(other.gameObject.tag == "HB") {
-
-                    GameObject GameClear_image = new GameObject("GameClearimage");
-                    GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameClear_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear");
-                    GameClear_image.GetComponent<Image>().preserveAspect = true;
-                    GameClear_image.GetComponent<Image>().SetNativeSize();
+                    GameClear();
                     Invoke("GoTitle",1.5f);
-                    
             }
 
             if(other.gameObject.tag == "HE") {
-
-                    GameObject GameOver_image = new GameObject("GameOverimage");
-                    GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
-                    GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
-                    GameOver_image.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-                    GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
-                    GameOver_image.GetComponent<Image>().preserveAspect = true;
-                    GameOver_image.GetComponent<Image>().SetNativeSize();
+                    GameOver();
                     Invoke("GoSelect_Human",1.5f);
-
             }
-    }
-
-    public void GoSelect() {
-        SceneManager.LoadScene("Select");
-    }
-
-    public void GoSelect_Bard() {
-       SceneManager.LoadScene("Select_Bard");
-    }
-
-    public void GoSelect_Cat() {
-        SceneManager.LoadScene("Select_Cat");
-    }
-
-    public void GoSelect_Human() {
-        SceneManager.LoadScene("Select_Human");
-    }
-
-    public void GoTitle() {
-        SceneManager.LoadScene("Title");
     }
 }
