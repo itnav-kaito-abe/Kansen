4a0818c19a83c54587e32a82b4a160802e570e03
kaito027 <kaito.san0.1818@gmail.com>
2018-09-19 23:16:49 +0900

    fix-Collition

--- kansen/Assets/Scripts/PlayerCollition.cs
+++ kansen/Assets/Scripts/PlayerCollition.cs
@@ -7,16 +7,19 @@ using UnityEngine.SceneManagement;
 public class PlayerCollition : MonoBehaviour {
 
 	// Use this for initialization
+
+    GameObject pc;
+
 	void Start () {
-		
+		pc = GameObject.Find("baikin_hana");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-     
-     public void GameClear() {
+
+    public void GameClear() {
         GameObject GameClear_image = new GameObject("GameClearimage");
         GameClear_image.transform.parent = GameObject.Find("Canvas").transform;
         GameClear_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
@@ -24,9 +27,15 @@ public class PlayerCollition : MonoBehaviour {
         GameClear_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/clear&nextstage");
         GameClear_image.GetComponent<Image>().preserveAspect = true;
         GameClear_image.GetComponent<Image>().SetNativeSize();
+        CircleCollider2D Collition = GameObject.Find("baikin_hana").gameObject.GetComponent<CircleCollider2D>();
+        CircleCollider2D Outside = GameObject.Find("baikin_hana").gameObject.transform.Find("Collition").gameObject.GetComponent<CircleCollider2D>();
+        Destroy(Collition);
+        Destroy(Outside);
+
+        pc.SetActive(false);
      }
 
-     public void GameOver() {
+    public void GameOver() {
         GameObject GameOver_image = new GameObject("GameOverimage");
         GameOver_image.transform.parent = GameObject.Find("Canvas").transform;
         GameOver_image.AddComponent<RectTransform>().anchoredPosition = new Vector3(0,0,-10);
@@ -34,68 +43,96 @@ public class PlayerCollition : MonoBehaviour {
         GameOver_image.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/UI/Combat/gameover");
         GameOver_image.GetComponent<Image>().preserveAspect = true;
         GameOver_image.GetComponent<Image>().SetNativeSize();
-     }
+        CircleCollider2D Collition = GameObject.Find("baikin_hana").gameObject.GetComponent<CircleCollider2D>();
+        CircleCollider2D Outside = GameObject.Find("baikin_hana").gameObject.transform.Find("Collition").gameObject.GetComponent<CircleCollider2D>();
+        Destroy(Collition);
+        Destroy(Outside);
 
-     public void GoSelect() {
-        SceneManager.LoadScene("Select");
-     }
+        pc.SetActive(false);
+    }
 
-     public void GoSelect_Bard() {
-        SceneManager.LoadScene("Select_Bard");
-     }
+    public void GoSelect() {
+    SceneManager.LoadScene("Select");
+    }
 
-     public void GoSelect_Cat() {
-        SceneManager.LoadScene("Select_Cat");
-     }
+    public void GoSelect_Bard() {
+    SceneManager.LoadScene("Select_Bard");
+    }
 
-     public void GoSelect_Human() {
-        SceneManager.LoadScene("Select_Human");
-     }
+    public void GoSelect_Cat() {
+    SceneManager.LoadScene("Select_Cat");
+    }
 
-     public void GoTitle() {
-        SceneManager.LoadScene("Title");
-     }
+    public void GoSelect_Human() {
+    SceneManager.LoadScene("Select_Human");
+    }
 
-     private void OnTriggerEnter2D(Collider2D other) {
-            if(other.gameObject.tag == "GB") {
-                    GameClear();
-                    Invoke("GoSelect_Bard",1.5f);
-            }
+    public void GoTitle() {
+    SceneManager.LoadScene("Title");
+    }
 
-            if(other.gameObject.tag == "GE") {
-                    GameOver();
-                    Invoke("GoSelect",1.5f);
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "GE") {
+                GameOver();
+                Invoke("GoSelect",1.5f);
+        }
 
-            }
+        if(collision.gameObject.tag == "BE") {
+                GameOver();
+                Invoke("GoSelect_Bard",1.5f);
+        }
 
-            if(other.gameObject.tag == "BB") {
-                    GameClear();
-                    Invoke("GoSelect_Cat",1.5f);
-            }
+        if(collision.gameObject.tag == "CE") {
+                GameOver();
+                Invoke("GoSelect_Cat",1.5f);
+        }
 
-            if(other.gameObject.tag == "BE") {
-                    GameOver();
-                    Invoke("GoSelect_Bard",1.5f);
-            }
+        if(collision.gameObject.tag == "HE") {
+                GameOver();
+                Invoke("GoSelect_Human",1.5f);
+        }
+    }
 
-            if(other.gameObject.tag == "CB") {
-                    GameClear();
-                    Invoke("GoSelect_Human",1.5f);       
-            }
+    private void OnTriggerEnter2D(Collider2D other) {
 
-            if(other.gameObject.tag == "CE") {
-                    GameOver();
-                    Invoke("GoSelect_Cat",1.5f);
-            }
+        if(other.gameObject.tag == "GB") {
+                GameClear();
+                Invoke("GoSelect_Bard",1.5f);
+        }
 
-            if(other.gameObject.tag == "HB") {
-                    GameClear();
-                    Invoke("GoTitle",1.5f);
-            }
+        if(other.gameObject.tag == "GBullet") {
+                GameOver();
+                Invoke("GoSelect",1.5f);
+        }
 
-            if(other.gameObject.tag == "HE") {
-                    GameOver();
-                    Invoke("GoSelect_Human",1.5f);
-            }
+        if(other.gameObject.tag == "BBullet") {
+                GameOver();
+                Invoke("GoSelect_Bard",1.5f);
+        }
+
+        if(other.gameObject.tag == "BB") {
+                GameClear();
+                Invoke("GoSelect_Cat",1.5f);
+        }
+
+        if(other.gameObject.tag == "CBullet") {
+                GameOver();
+                Invoke("GoSelect_Cat",1.5f);
+        }
+
+        if(other.gameObject.tag == "CB") {
+                GameClear();
+                Invoke("GoSelect_Human",1.5f);       
+        }
+
+        if(other.gameObject.tag == "HBullet") {
+                GameOver();
+                Invoke("GoSelect_Human",1.5f);
+        }
+
+        if(other.gameObject.tag == "HB") {
+                GameClear();
+                Invoke("GoTitle",1.5f);
+        }
     }
 }
